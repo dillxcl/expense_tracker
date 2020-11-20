8536002db9e03f8892174ffa051c8fdf219fa827
dillxcl <49700091+dillxcl@users.noreply.github.com>
2020-11-19 17:58:35 -0800

    Update on expense app frontend and models

--- expense_app/admin.py
+++ expense_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Year_Expense
 # Register your models here.
+
+admin.site.register(Year_Expense)
--- expense_app/models.py
+++ expense_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Year_Expense(models.Model):
+    # name - What Year (front end option to select what year)
+    # budget - annual salary
+    year = models.CharField(max_length=100)
+    annual_salary = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return self.name
+
+class Month_Expense(models.Model):
+    # name - Month (auto generate 12 months)
+    # year_expense = models.ForiegnKey(Year_Expense, on_delete=models.CASCADE)
+    pass
+
+class Daily_Expense(models.Model):
+    # category - which items I have spent on 
+    # spent - which much money you have spent
+    pass
